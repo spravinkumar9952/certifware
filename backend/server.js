2455da7f0ed09fcfe18f0d2966b9ea3974330329
Rammprashath <prashathramm@gmail.com>
2023-03-21 14:54:11 +0530

    fixed reload after upload

--- backend/server.js
+++ backend/server.js
@@ -89,10 +89,10 @@ const imageStorage = multer.diskStorage({
 const imageUpload = multer({
     storage: imageStorage,
     limits: {
-        fileSize: 1000000 // 1000000 Bytes = 1 MB
+        fileSize: 4000000 // 1000000 Bytes = 1 MB
     },
     fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(png|jpg|pdf)$/)) {
+        if (!file.originalname.match(/\.(png|jpg)$/)) {
             // upload only png and jpg format
             return cb(new Error('Please upload a Image'))
         }
